b4d25bcda93abeeaa136e66810be3da7f5cd4314
David Lifschitz <53435994+davidlifschitz@users.noreply.github.com>
2026-08-20 13:18:15 -0400

    Refresh Pattern Foundry offline cache for mobile UX fix

--- games/pattern-foundry/sw.js
+++ games/pattern-foundry/sw.js
@@ -1,4 +1,4 @@
-const CACHE='pattern-foundry-traingames-v1';
+const CACHE='pattern-foundry-traingames-v2';
 const ASSETS=['./','./index.html','./styles.css','./engine.js','./game.js','./manifest.webmanifest','./train-games-icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
