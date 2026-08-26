90a73518b126bfc61efdec97039658882fed6799
David Lifschitz <53435994+davidlifschitz@users.noreply.github.com>
2026-08-26 01:29:24 -0400

    pwa: cache shared gameplay UI in Tilebound

--- games/tilebound/sw.js
+++ games/tilebound/sw.js
@@ -1,5 +1,5 @@
-const CACHE='tilebound-v1';
-const ASSETS=['./','./index.html','./THIRD_PARTY_NOTICES.md'];
+const CACHE='tilebound-v2';
+const ASSETS=['./','./index.html','./THIRD_PARTY_NOTICES.md','../gameplay-ui.css','../gameplay-ui.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',event=>{if(event.request.method==='GET')event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request)))});
