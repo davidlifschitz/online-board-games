0d84527754f94f514bdb5072e28d9767f095afec
David Lifschitz <53435994+davidlifschitz@users.noreply.github.com>
2026-08-26 01:58:26 -0400

    fix: scope Tilebound cache cleanup

--- games/tilebound/sw.js
+++ games/tilebound/sw.js
@@ -1,5 +1,5 @@
 const CACHE='tilebound-v2';
 const ASSETS=['./','./index.html','./THIRD_PARTY_NOTICES.md','../gameplay-ui.css','../gameplay-ui.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('tilebound-')&&key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method==='GET')event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request)))});
